eabf83420943292a9c0b747d2a64195ce32faa44
kimzerovirus <yg9699k@naver.com>
2023-04-28 14:05:46 +0900

    선택 정렬

---
+++ java/src/me/kzv/sort/SelectionSort.java
@@ -0,0 +1,30 @@
+package me.kzv.sort;
+
+public class SelectionSort {
+    public static void main(String[] args) {
+        selection();
+    }
+
+    static void selection() {
+        int[] A = new int[]{1, 5, 2, 3, 4};
+
+        for (int i = 0; i < A.length; i++) {
+            int Minimum = i; // 순회를 돌면서 가장 작은 값을 담을 변수
+
+            // 맨 앞은 정렬된 부분이므로 하나씩 뒤로 정렬범위를 좁힌다
+            for (int j = i + 1; j < A.length; j++) {
+                if (A[j] < A[Minimum]) Minimum = j;
+            }
+
+            if (A[i] > A[Minimum]) {
+                int temp = A[i];
+                A[i] = A[Minimum];
+                A[Minimum] = temp;
+            }
+        }
+
+        for (int i : A) {
+            System.out.println(i);
+        }
+    }
+}
