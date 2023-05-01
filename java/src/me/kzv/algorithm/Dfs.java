bc643332340799fecd46b8b9517ad6e3094fe836
kimzerovirus <yg9699k@naver.com>
2023-05-02 01:35:28 +0900

    dfs구현

--- java/src/me/kzv/algorithm/Dfs.java
+++ java/src/me/kzv/algorithm/Dfs.java
@@ -1,5 +1,8 @@
 package me.kzv.algorithm;
 
+import java.util.Arrays;
+import java.util.Stack;
+
 public class Dfs {
     // DFS 깊이 우선 탐색
     // 스택, 재귀 사용
@@ -12,5 +15,120 @@ public class Dfs {
         // 4. 인접 노드를 순서대로 스택에 저장
         // 5. 스택이 빌 때까지 pop 하고 인접 노드를 스택에 저장하길 무한 반복
 
+        // 트리 구현
+
+        /*
+                 0
+                / \
+               1   2
+              / \   \
+             3   4 - 5
+             \      /
+              \    /
+               \  /
+                 6
+        */
+        int[][] map = {
+                {0, 1},
+                {0, 2},
+                {1, 3},
+                {1, 4},
+                {2, 5},
+                {3, 6},
+                {4, 5},
+                {5, 6},
+        };
+
+        Stack<Integer> st = new Stack<>();
+        int[] visited = new int[7];
+
+        int startNode = 0;
+        st.push(startNode);
+        visited[startNode] = 1;
+
+        // DFS
+        while (!st.isEmpty()) {
+            int now = st.pop();
+            for (int i = 0; i < map.length; i++) {
+                if (map[i][0] == now) {
+                    int next = map[i][1];
+                    if (visited[next] == 0) {
+                        st.push(next);
+                        visited[next] = visited[now] + 1;
+                    }
+                }
+            }
+
+            System.out.println("pop 추출: " + now);
+            System.out.println("stack 인접 노드 : " + st);
+            System.out.println("방문 : " + Arrays.toString(visited));
+            System.out.println("------------------");
+        }
+    }
+}
+
+class DFS_Recursion {
+    static boolean[] visited = new boolean[9];
+
+    // 그래프의 연결상태를 2차원 배열로 표현
+    // 인덱스가 각각의 노드번호가 될 수 있게 0번인덱스는 아무것도 없는 상태
+    static int[][] graph = {{}, {2, 3, 8}, {1, 6, 8}, {1, 5}, {5, 7}, {3, 4, 7}, {2}, {4, 5}, {1, 2}};
+
+    public static void main(String[] args) {
+        dfs(1);
+    }
+
+    static void dfs(int nodeIndex) {
+        // 방문 처리
+        visited[nodeIndex] = true;
+
+        // 방문 노드 출력
+        System.out.print(nodeIndex + " -> ");
+
+        // 방문한 노드에 인접한 노드 찾기
+        for (int node : graph[nodeIndex]) {
+            // 인접한 노드가 방문한 적이 없다면 DFS 수행
+            if (!visited[node]) {
+                dfs(node);
+            }
+        }
+    }
+}
+
+class DFS_stack {
+
+    // 방문처리에 사용 할 배열선언
+    static boolean[] visited = new boolean[9];
+
+    // 그림예시 그래프의 연결상태를 2차원 배열로 표현
+    // 인덱스가 각각의 노드번호가 될 수 있게 0번인덱스는 아무것도 없는 상태
+    static int[][] graph = {{}, {2, 3, 8}, {1, 6, 8}, {1, 5}, {5, 7}, {3, 4, 7}, {2}, {4, 5}, {1, 2}};
+
+    static Stack<Integer> stack = new Stack<>();
+
+    public static void main(String[] args) {
+
+        // 시작 노드 세팅
+        stack.push(1);
+        visited[1] = true;
+
+        // 스택이 비어있지 않으면 계속 반복
+        while (!stack.isEmpty()) {
+
+            // 스택에서 하나 추출
+            int nodeIndex = stack.pop();
+
+            // 방문 노드 출력
+            System.out.print(nodeIndex + " -> ");
+
+            // 꺼낸 노드와 인접한 노드 찾기
+            for (int LinkedNode : graph[nodeIndex]) {
+                // 인접한 노드를 방문하지 않았을 경우에 스택에 넣고 방문처리
+                if (!visited[LinkedNode]) {
+                    stack.push(LinkedNode);
+                    visited[LinkedNode] = true;
+                }
+            }
+        }
     }
 }
