ca9012cd3e72dbbfcf95c62925baea7191eb43ed
kimzerovirus <yg9699k@naver.com>
2023-05-07 22:03:47 +0900

    우테캠 코테 다시 풀어보기

--- java/src/me/kzv/programers/우캠네트워크4.java
+++ java/src/me/kzv/programers/우캠네트워크4.java
@@ -1,8 +1,8 @@
 package me.kzv.programers;
 
 import java.util.Arrays;
-import java.util.Comparator;
 import java.util.List;
+import java.util.Stack;
 
 /**
  * 크루스칼 알고리즘 (Kruskal Algorithm)
@@ -16,57 +16,84 @@ import java.util.List;
  */
 // 처음에 네트워크랑 리패어 돌면서 하나라도 연결되지 않을땐 탐색없이 리턴하고 종료
 public class 우캠네트워크4 {
-
-}
-
-class Solution {
-    static int solution(int[][] network, int[][] recover) {
-        int answer = -1;
-        int[][] graph = new int[network.length + recover.length][3];
-        for (int i = 0; i < network.length; i++) {
-            graph[i] = new int[]{network[i][0], network[i][1], 0};
-        }
-        for (int j = 0; j < recover.length; j++) {
-            graph[j + network.length] = recover[j];
-        }
-
-        // 간선 비용 순으로 오름차순 정렬
-        Arrays.sort(graph, Comparator.comparingInt(o -> o[2]));
-
-        kruskal(graph, new int[]{1, 2, 3, 4, 5, 6});
-        return answer;
-    }
-
-    static void union(int[] parent, int x, int y) {
-        x = find(parent, x);
-        y = find(parent, y);
-
-        if (x < y) parent[y] = x;
-        else parent[x] = y;
-    }
-
-    static int find(int[] parent, int i) {
-        if (parent[i] == i) return i;
-        else return find(parent, parent[i]);
-    }
-
-    static void kruskal(int[][] graph, int[] parent) {
-        int cost = 0;
-
-        for (int[] g : graph) {
-            if (find(parent, g[0]) != find(parent, g[1])) {
-                cost += g[2];
-                union(parent, g[0], g[1]);
+    class Solution {
+        static int solution(int[][] network, int[][] recover) {
+            int[][] graph = new int[network.length + recover.length][3];
+            for (int i = 0; i < network.length; i++) {
+                graph[i] = new int[]{network[i][0], network[i][1], 0}; // 이미 연결된 부분은 비용을 0으로 넣고 진행?
             }
+            for (int j = 0; j < recover.length; j++) {
+                graph[j + network.length] = recover[j];
+            }
+
+            // 간선 비용 순으로 오름차순 정렬
+            Arrays.sort(graph, (o1, o2) -> Integer.compare(o1[2], o2[2]));
+
+            int[] parent = new int[]{1, 2, 3, 4, 5, 6}; // 아마 연결해야할 네트워크 총 개수를 제공했던것 같음
+
+            if (!isConnected(graph)) return -1; // 노드가 연결되어 있지 않음
+            else return kruskal(graph, parent);
         }
 
-        System.out.println(cost);
-    }
+        static void union(int[] parent, int x, int y) {
+            x = find(parent, x);
+            y = find(parent, y);
 
-    public static void main(String[] args) {
-        int[][] network = new int[][]{{1, 2}, {2, 3}, {4, 5}, {5, 6}};
-        int[][] recover = new int[][]{{2, 4, 10}, {3, 5, 15}};
+            if (x < y) parent[y - 1] = x;
+            else parent[x - 1] = y;
+        }
 
-        solution(network, recover);
+        static int find(int[] parent, int i) {
+            if (parent[i - 1] == i) return i;
+            else return find(parent, parent[i - 1]);
+        }
+
+        static int kruskal(int[][] graph, int[] parent) {
+            int cost = 0;
+
+            for (int[] g : graph) {
+                if (find(parent, g[0]) != find(parent, g[1])) {
+                    cost += g[2];
+                    union(parent, g[0], g[1]);
+                }
+            }
+            return cost;
+        }
+
+        static boolean isConnected(int[][] graph) {
+            Stack<Integer> st = new Stack<>();
+            int[] visited = new int[6];
+
+            st.push(graph[0][0]);
+            visited[0] = 1;
+
+            while (!st.isEmpty()) {
+                int now = st.pop();
+                for (int i = 0; i < graph.length; i++) {
+                    if (graph[i][0] == now) {
+                        int next = graph[i][1];
+                        if (visited[next - 1] == 0) {
+                            st.push(next);
+                            visited[next - 1] = 1;
+                        }
+                    }
+                }
+            }
+
+            for (int i : visited) {
+                if (i == 0) return false; // 한번이라도 방문하지 않은 노드가 있음 = 트리가 끊어져 있음
+            }
+
+            return true;
+        }
+
+        public static void main(String[] args) {
+            int[][] network = new int[][]{{1, 2}, {2, 3}, {4, 5}, {5, 6}};
+            int[][] recover = new int[][]{{3, 4, 10}, {2, 6, 15}, {1, 3, 12}};
+
+            int answer = solution(network, recover);
+            System.out.println("answer = " + answer);
+        }
     }
 }
+
