ee5cac5e29740c14284a8cc05f25c0aa59f37cb0
kimzerovirus <yg9699k@naver.com>
2023-05-17 20:51:12 +0900

    거리두기 주석 추가

--- java/src/me/kzv/programers/거리두기확인하기.java
+++ java/src/me/kzv/programers/거리두기확인하기.java
@@ -22,7 +22,7 @@ public class 거리두기확인하기 {
         for (int y = 0; y < 5; y++) {
             for (int x = 0; x < 5; x++) {
                 if (place[y].charAt(x) == 'P') {
-                    if (bfs(place, y, x)) {
+                    if (bfs(place, y, x)) { // 거리두기를 지키지 않고 있음
                         return false;
                     }
                 }
@@ -46,7 +46,8 @@ public class 거리두기확인하기 {
 
                 int d = Math.abs(y - ny) + Math.abs(x - nx); // 맨해튼 거리
 
-                // 맨해튼 거리 1인 지점에 o가 나온다면 o를 기준으로 탐색을 한번 더 한다
+                // X는 더이상 탐색을 하지 않는다 -> PXP 의 경우 거리두기를 지킨것이므로
+                // 맨해튼 거리 1인 지점에 O가 나온다면 O를 기준으로 탐색을 한번 더 한다 -> 다음 회차에서 POP 가 나오는 경우를 찾는다
                 if (place[ny].charAt(nx) == 'O' && d == 1) {
                     q.add(new int[]{ny, nx});
                 } else if (place[ny].charAt(nx) == 'P' && d <= 2) {
