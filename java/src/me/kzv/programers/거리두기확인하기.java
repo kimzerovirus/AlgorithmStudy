9e44b4df0355d08164b0d7de1e2fbfe3b6598420
kimzerovirus <yg9699k@naver.com>
2023-05-17 20:30:05 +0900

    거리두기 확인하기

---
+++ java/src/me/kzv/programers/거리두기확인하기.java
@@ -0,0 +1,59 @@
+package me.kzv.programers;
+
+import java.util.*;
+
+public class 거리두기확인하기 {
+    final int[] dy = {-1, 0, 1, 0};
+    final int[] dx = {0, 1, 0, -1};
+
+    public int[] solution(String[][] places) {
+        int[] answer = new int[5];
+        for (int i = 0; i < 5; i++) {
+            if (check(places[i])) {
+                answer[i] = 1;
+            } else {
+                answer[i] = 0;
+            }
+        }
+        return answer;
+    }
+
+    private boolean check(String[] place) {
+        for (int y = 0; y < 5; y++) {
+            for (int x = 0; x < 5; x++) {
+                if (place[y].charAt(x) == 'P') {
+                    if (bfs(place, y, x)) {
+                        return false;
+                    }
+                }
+            }
+        }
+        return true;
+    }
+
+    private boolean bfs(String[] place, int y, int x) {
+        Queue<int[]> q = new LinkedList<>();
+        q.add(new int[]{y, x});
+
+        while (!q.isEmpty()) {
+            int[] point = q.poll();
+            for (int i = 0; i < 4; i++) {
+                int ny = point[0] + dy[i]; // 탐색할 지점의 행 번호
+                int nx = point[1] + dx[i]; // 탐색할 지점의 열 번호
+
+                if (ny < 0 || nx < 0 || ny >= 5 || nx >= 5) continue; // 범위 벗어나면 넘어가기
+                if (ny == y && nx == x) continue; // 새로 탐색하는 지점이 최초 지점이면 넘어가기
+
+                int d = Math.abs(y - ny) + Math.abs(x - nx); // 맨해튼 거리
+
+                // 맨해튼 거리 1인 지점에 o가 나온다면 o를 기준으로 탐색을 한번 더 한다
+                if (place[ny].charAt(nx) == 'O' && d == 1) {
+                    q.add(new int[]{ny, nx});
+                } else if (place[ny].charAt(nx) == 'P' && d <= 2) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
