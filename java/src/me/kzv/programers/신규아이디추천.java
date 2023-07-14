998396180efbee2fee794eb0036588f00404b282
kimzerovirus <yg9699k@naver.com>
2023-07-14 23:42:44 +0900

    신규 아이디 추천

---
+++ java/src/me/kzv/programers/신규아이디추천.java
@@ -0,0 +1,23 @@
+package me.kzv.programers;
+
+public class 신규아이디추천 {
+    class Solution {
+        public String solution(String new_id) {
+            new_id = new_id.toLowerCase().replaceAll("[^a-z0-9-_.]", "");
+
+            while (new_id.contains("..")) new_id = new_id.replace("..", ".");
+
+            if (new_id.length() > 0 && new_id.charAt(0) == '.') new_id = new_id.substring(1, new_id.length());
+            if (new_id.length() > 15) new_id = new_id.substring(0, 15);
+            if (new_id.length() > 0 && new_id.charAt(new_id.length() - 1) == '.')
+                new_id = new_id.substring(0, new_id.length() - 1);
+
+            if (new_id.length() == 0) new_id = "a";
+            if (new_id.length() <= 2) {
+                while (new_id.length() < 3) new_id += new_id.charAt(new_id.length() - 1);
+            }
+
+            return new_id;
+        }
+    }
+}
