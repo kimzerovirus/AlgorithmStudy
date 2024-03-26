d6ca4c1ac766b037eed4c91632abaf7415183b81
kimzerovirus <yg9699k@naver.com>
2024-03-26 21:07:55 +0900

    시험감독

--- java/src/me/kzv/baekjoon/B13458시험감독.java
+++ java/src/me/kzv/baekjoon/B13458시험감독.java
@@ -1,4 +1,34 @@
 package me.kzv.baekjoon;
 
+import java.util.*;
+
 public class B13458시험감독 {
+    // int 범위를 초과하는 입력값이 있음..
+    private static long result = 0;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int[] arr = new int[N];
+
+        for (int i = 0; i < N; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int B = sc.nextInt();
+        int C = sc.nextInt();
+
+        for (int i = 0; i < N; i++) {
+            int students = arr[i];
+
+            students -= B;
+            result += 1;
+            if (students <= 0) continue;
+
+            result += students / C;
+            if (students % C > 0) result += 1;
+        }
+
+        System.out.println(result);
+    }
 }
